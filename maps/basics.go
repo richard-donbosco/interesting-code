3130a2bcd4a0530bcdd59164a988ab30252232ce
ricdon41 <ricdon41@gmail.com>
2023-02-11 14:14:53 -0500

    Improve intersection

--- maps/basics.go
+++ maps/basics.go
@@ -12,10 +12,7 @@ func main() {
 	add_to_map(ages, "daniel", 31)
 	add_to_map(ages, "michael", 45)
 
-	for k, v := range ages {
-		fmt.Println(k, " ", v)
-	}
-
+	map_print(ages)
 	males := make(map[string]int)
 
 	add_to_map(males, "Arnold", 19)
@@ -23,10 +20,38 @@ func main() {
 	add_to_map(males, "Joseph", 41)
 	add_to_map(males, "Richard", 28)
 
+	map_print(males)
+
 	// check if males and and ages have same values
+	var matching_keys map[string]int
+	matching_keys = intersect(males, ages)
+	map_print(matching_keys)
 
-	intersect(males, ages)
+}
 
+func map_print(a map[string]int) {
+	fmt.Println("key\tvalue")
+	for k, v := range a {
+		fmt.Println(fmt.Sprintf("%s\t%d", k, v))
+	}
+}
+
+func map_print_matching_keypairs(a map[string]int, keys []string) {
+	fmt.Println("key\tvalue")
+	for k, v := range a {
+		if slice_contains(keys, k) {
+			fmt.Println(fmt.Sprintf("%s\t%d", k, v))
+		}
+	}
+}
+
+func slice_contains(a []string, value string) bool {
+	for _, v := range a {
+		if v == value {
+			return true
+		}
+	}
+	return false
 }
 
 func add_to_map(a map[string]int, k string, v int) {
@@ -47,11 +72,12 @@ func delete_from_map(a map[string]int, k string) {
 	}
 }
 
-func intersect(a, b map[string]int) {
-
+func intersect(a, b map[string]int) map[string]int {
+	matching_keys := make(map[string]int)
 	for k, v := range a {
 		if _, ok := b[k]; ok {
 			if v == b[k] {
+				matching_keys[k] = v
 				fmt.Println(k, " exists in both maps and ages, value is ", b[k])
 			} else {
 				fmt.Println("age of ", k, " is different in ages and males")
@@ -61,5 +87,5 @@ func intersect(a, b map[string]int) {
 			fmt.Println(k, " doesn't exist in ages")
 		}
 	}
-
+	return matching_keys
 }
