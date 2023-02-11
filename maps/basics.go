f0878e75926a8a821dd256a7940a9b9a002fed8b
ricdon41 <ricdon41@gmail.com>
2023-02-11 13:50:29 -0500

    Add interfacing function to maps

--- maps/basics.go
+++ maps/basics.go
@@ -2,40 +2,49 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func main() {
 	ages := make(map[string]int)
 
-	ages["richard"] = 28
-	ages["daniel"] = 31
-	ages["michael"] = 45
+	add_to_map(ages, "richard", 28)
+	add_to_map(ages, "daniel", 31)
+	add_to_map(ages, "michael", 45)
 
 	for k, v := range ages {
-		fmt.Printf(k)
-		fmt.Println(v)
-
+		fmt.Println(k, " ", v)
 	}
 
 	males := make(map[string]int)
 
-	males["Arnold"] = 19
-	males["Tim"] = 29
-	males["Joseph"] = 41
-	males["Richard"] = 28
+	add_to_map(males, "Arnold", 19)
+	add_to_map(males, "Tim", 29)
+	add_to_map(males, "Joseph", 41)
+	add_to_map(males, "Richard", 28)
 
 	// check if males and and ages have same values
 
 	for k, v := range males {
 		if _, ok := ages[k]; ok {
 			if v == ages[k] {
-				fmt.Println(k, " exists in both maps and ages is ", ages[k])
+				fmt.Println(k, " exists in both maps and ages, value is ", ages[k])
 			} else {
-				fmt.Println("age of ", k, "is different in ages and males")
+				fmt.Println("age of ", k, " is different in ages and males")
 			}
 
 		} else {
 			fmt.Println(k, " doesn't exist in ages")
 		}
+	}
+
+}
+
+func add_to_map(a map[string]int, k string, v int) {
+	k = strings.ToLower(k)
+	if _, ok := a[k]; !ok {
+		a[k] = v
+	} else {
+		fmt.Println(k, " already exists in the map")
 	}
 }
