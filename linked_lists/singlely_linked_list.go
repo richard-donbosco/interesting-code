5f0f5b2a519337c336fbabea8a599cef209c6691
ricdon41 <ricdon41@gmail.com>
2023-02-11 21:56:50 -0500

    Singly link list

--- linked_lists/singlely_linked_list.go
+++ linked_lists/singlely_linked_list.go
@@ -17,12 +17,17 @@ type list struct {
 func main() {
 	var list1 *list = &list{}
 
-	for i := 0; list1.len() < 5; i++ {
-		list1.unique_append(rand.Intn(6))
+	//append large number of random values
+	for i := 0; i < 100; i++ {
+		list1.unique_append(rand.Intn(1000))
 	}
 
 	list1.print()
-	// list1.find()
+	fmt.Println("---------")
+
+	list1.sort()
+	list1.print()
+	fmt.Println("---------")
 
 }
 
@@ -59,7 +64,7 @@ func (l *list) len() int {
 }
 
 func (l *list) unique_append(val int) {
-	if !(l.find(val)) {
+	if !(l.exists(val)) {
 		if l.head == nil {
 			l.head = &node{value: val, next: nil}
 		} else {
@@ -87,21 +92,23 @@ func (l *list) print() {
 				item = *item.next
 			}
 		}
+	} else {
+		fmt.Println("The list is empty")
 	}
 }
 
-func (l *list) find(val int) bool {
+func (l *list) exists(val int) bool {
 	var found bool
 	if l.head != nil {
 		var item node = *l.head
 		for {
 			if item.value == val {
-				fmt.Printf("The value %v exists in the list\n", val)
+				// fmt.Printf("The value %v exists in the list\n", val)
 				found = true
 			}
 			if item.next == nil {
 				if !found {
-					fmt.Printf("The value doesn't exist\n")
+					// fmt.Printf("The value doesn't exist\n")
 					found = false
 				}
 				break
@@ -111,4 +118,73 @@ func (l *list) find(val int) bool {
 		}
 	}
 	return found
+}
+
+func (l *list) smallest() (int, bool) {
+	if l.len() == 0 {
+		fmt.Println("The list is empty")
+		return 0, false
+	}
+	var smallest int
+	if l.head != nil {
+		var item node = *l.head
+		smallest = item.value
+		for {
+			if item.next != nil {
+				if item.next.value < smallest {
+					smallest = item.next.value
+				}
+			} else {
+				break
+			}
+			item = *item.next
+		}
+	}
+	return smallest, true
+}
+
+func (l *list) sort() {
+	var list2 *list = &list{}
+	var length int = l.len()
+	for i := 1; i <= length; i++ {
+		val, _ := l.smallest()
+		list2.unique_append(val)
+		l.remove(val)
+	}
+	l.head = list2.head
+}
+
+func (l *list) remove(val int) (int, bool) {
+	if l.head == nil {
+		return 0, false
+	}
+	if l.head.value == val {
+		if l.head.next != nil {
+			l.head = l.head.next
+		} else {
+			l.head = nil
+		}
+		return val, true
+	}
+
+	var current *node = l.head
+	for {
+		if current.next != nil {
+			if current.next.value == val {
+				if current.next.next != nil {
+					current.next = current.next.next
+				} else {
+					current.next = nil
+				}
+				return val, true
+			} else {
+				current = current.next
+			}
+		} else {
+			break
+		}
+	}
+
+	return 0, false
+
 }
