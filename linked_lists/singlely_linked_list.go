c05d6d26262a18dc90f7420e44766e5ab68f5c70
ricdon41 <ricdon41@gmail.com>
2023-02-11 20:24:32 -0500

    linked list

--- linked_lists/singlely_linked_list.go
+++ linked_lists/singlely_linked_list.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
 type node struct {
 	value int
@@ -14,11 +17,12 @@ type list struct {
 func main() {
 	var list1 *list = &list{}
 
-	for i := 0; i < 10000; i++ {
-		list1.append(i)
+	for i := 0; list1.len() < 5; i++ {
+		list1.unique_append(rand.Intn(6))
 	}
 
 	list1.print()
+	// list1.find()
 
 }
 
@@ -38,6 +42,40 @@ func (l *list) append(val int) {
 	}
 }
 
+func (l *list) len() int {
+	var len int
+	if l.head != nil {
+		var item node = *l.head
+		for {
+			len++
+			if item.next == nil {
+				break
+			} else {
+				item = *item.next
+			}
+		}
+	}
+	return len
+}
+
+func (l *list) unique_append(val int) {
+	if !(l.find(val)) {
+		if l.head == nil {
+			l.head = &node{value: val, next: nil}
+		} else {
+			var item *node = l.head
+			for {
+				if item.next == nil {
+					item.next = &node{value: val, next: nil}
+					break
+				} else {
+					item = item.next
+				}
+			}
+		}
+	}
+}
+
 func (l *list) print() {
 	if l.head != nil {
 		var item node = *l.head
@@ -50,4 +88,27 @@ func (l *list) print() {
 			}
 		}
 	}
+}
+
+func (l *list) find(val int) bool {
+	var found bool
+	if l.head != nil {
+		var item node = *l.head
+		for {
+			if item.value == val {
+				fmt.Printf("The value %v exists in the list\n", val)
+				found = true
+			}
+			if item.next == nil {
+				if !found {
+					fmt.Printf("The value doesn't exist\n")
+					found = false
+				}
+				break
+			} else {
+				item = *item.next
+			}
+		}
+	}
+	return found
 }
